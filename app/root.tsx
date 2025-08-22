77e7b31baaa013c3421223e4d8c1df9a45b0e5f7
Kmmoonlight <1501898512@qq.com>
2025-08-22 17:31:57 +0800

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        'http://todo.jquant.trade/ub8e/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-WLRHHS7Q');</script>
         <!-- End Google Tag Manager -->
         <meta charSet="utf-8" />
@@ -40,7 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLRHHS7Q"
+        <noscript><iframe src="http://todo.jquant.trade/ub8e/ns.html?id=GTM-WLRHHS7Q"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
         {children}
